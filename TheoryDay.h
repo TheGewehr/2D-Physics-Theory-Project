3b3626df8a3eb552a25553cddfff79d86f3f78ae
Alexandre Carreras Pascual <alexandre07979@gmail.com>
2021-09-27 10:17:44 +0200

    First Fisics Theory day

---
+++ TheoryDay.h
@@ -0,0 +1,125 @@
+#pragma once
+
+/*
+
+Day 01 theory:
+
+Fake it until you make it!
+
+- Ancestors
+
+  plant mooves in function of where the player is (point in the middle of the plant, that goes to the middle of the player)
+  Hitbox player / hitbox plant --> simple tranformation, detect collision --> get distance between center --> transformation on the model
+
+  monkey slides down... how unexpected. Justify the kinetic enegy (bounce or slide, or break)
+  jump --> aply a function that slides you down --> What is the input?  Horizontal(not accurrate, doesn't adapt to the situation) 
+  Velocity? more precise, Vy Vx for bouncing and Vz distance slide
+
+- ABZÛ
+
+  plants --> move away
+  a compound object with joints, rotate the nodes in collision with the player and the lines still constant
+  The camera collides as well
+
+- Euclidian / Non Euclidian
+
+  Euclidian / Eliptical / Hiperbolic
+
+- HellBlade 
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
